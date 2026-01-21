d99b3836889dbe814683dc5b6dfc74a90c846526
Florencio <florencio.pool@hotmail.com>
2026-01-21 10:54:15 -0600

    add code generator

--- src/main/java/com/example/shortener/CodeGenerator.java
+++ src/main/java/com/example/shortener/CodeGenerator.java
@@ -1,4 +1,18 @@
 package com.example.shortener;
 
+import java.security.SecureRandom;
+
 public class CodeGenerator {
+    private static final String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final SecureRandom RNG = new SecureRandom();
+
+    public static String generate(int length){
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i ++){
+            sb.append(ALPHABET.charAt(RNG.nextInt(ALPHABET.length())));
+        }
+
+        return sb.toString();
+    }
 }
